c82ee016d41ef9aeada90d1e307cf79da1835777
ctil <ctil@users.noreply.github.com>
2026-02-01 10:31:02 -0700

    Make first migration idempotent

--- drizzle/0000_known_goliath.sql
+++ drizzle/0000_known_goliath.sql
@@ -1,12 +1,12 @@
-CREATE TABLE `portfolios` (
+CREATE TABLE IF NOT EXISTS `portfolios` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
 	`description` text,
 	`created_at` text DEFAULT 'datetime(''now'')'
 );
 --> statement-breakpoint
-CREATE UNIQUE INDEX `portfolios_name_unique` ON `portfolios` (`name`);--> statement-breakpoint
-CREATE TABLE `stock_allocations` (
+CREATE UNIQUE INDEX IF NOT EXISTS `portfolios_name_unique` ON `portfolios` (`name`);--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS `stock_allocations` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`portfolio_id` integer NOT NULL,
 	`symbol` text NOT NULL,
